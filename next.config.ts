de63eb333de414c43b2a38295eb4d775e12ebc1b
Kiki <kikimworkmail@gmail.com>
2025-05-14 20:50:52 -0400

    azure settings

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    distDir: 'build',
+    output: 'standalone',
+  };
+  
+  export default nextConfig;
